5e224c271a0ec7897293c1769e463a1f39f651c5
killthebee <danny.developer@ro.ru>
2023-08-31 20:50:22 +0300

    Initial Commit

---
+++ UnsplashClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnsplashClient
+//
+//  Created by admin on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
